de725f4a1db42c460e1cd7adae2a2d077926eda2
TranTam <tamvaa1306@gmail.com>
2022-09-26 22:07:12 +0700

    basic AntD Button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="home">
     <h1>This is an home page</h1>
+    <a-button>Add</a-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { Button } from "ant-design-vue";
+// const ButtonGroup = Button.Group;
+
 export default defineComponent({
   name: "HomeView",
+  components: {
+    AButton: Button,
+    // AButtonGroup: ButtonGroup,
+  },
 });
 </script>
