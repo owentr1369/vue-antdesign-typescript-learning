3fee70d03e13dba038ab57866915b39a60c55400
TranTam <tamvaa1306@gmail.com>
2022-09-26 21:47:17 +0700

    setup vue and ts

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>This is an home page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HomeView",
+});
+</script>
